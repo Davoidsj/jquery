a604327fd1b9a36eb77b95ef44ebf28cd0be5da9
Davoidsj <alokpathik56@gmail.com>
2023-09-03 01:36:10 +0600

    new file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-$("h1").addClass("style1 style2");
+let result = $('#display');
+
+
+$('.button-text').click((e)=>{
+let existingNumber = result.val();
+let value = e.target.value;
+result.val(existingNumber+value);
+})
+
+$('#percentage').click(()=>{
+    result.val(Number(result.val())/100);
+})
+
+$('#clear').click(()=>{
+    result.val('');
+})
+
+$('#delete').click(()=>{
+    let previousValue = result.val();
+    if(previousValue!==' '){}
+    result.val(previousValue.slice(0,-1));
+})
+
+$('#equal').click(()=>{
+    let newResult = eval(result.val());
+    result.val(newResult);
+})
